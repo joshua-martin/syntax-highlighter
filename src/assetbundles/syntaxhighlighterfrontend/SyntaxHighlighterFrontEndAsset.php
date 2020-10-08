3b75f83ab977a2d6f8c45b33c030e0543ebc376a
Joshua Martin <hello@joshuamartin.co.uk>
2020-10-08 10:42:32 +0100

    correct route

--- src/assetbundles/syntaxhighlighterfrontend/SyntaxHighlighterFrontEndAsset.php
+++ src/assetbundles/syntaxhighlighterfrontend/SyntaxHighlighterFrontEndAsset.php
@@ -29,7 +29,7 @@ class SyntaxHighlighterFrontEndAsset extends AssetBundle
      */
     public function init()
     {
-        $this->sourcePath = "@hashtagerrors/syntaxhighlighter/assetbundles/syntaxhighlighter/dist";
+        $this->sourcePath = "@hashtagerrors/syntaxhighlighter/assetbundles/syntaxhighlighterfrontend/dist";
         
         $this->js = [
             'prism.js',
